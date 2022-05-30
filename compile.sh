d42d3524a61eeb3d3adf68f60610a491443a56c3
yzouchen <854469629@qq.com>
2022-05-30 23:38:56 +0800

    add compile.sh

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ["cd", "./fs/fs"],
+# ["make", "clean"],
+# ["make"],
+# ["cd", "./tests"],
+# ["bash", "./test.sh"],
+# ["E"]
+
+cd ./fs/fs || exit
+rm build -rf
+mkdir build && cd build && cmake .. && make
+cd ../tests || exit
+./tests.sh
